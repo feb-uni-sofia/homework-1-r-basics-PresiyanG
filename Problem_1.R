0a1d5ad8c1b5c2c717953f3a89da6b5797d2d74a
Presiyan <presian.georgiev@outlook.com>
2018-04-01 18:25:10 +0200

    asd

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,25 @@
+# a
+x <- c(4, 1, 1, 4)
 
+#b
+y <- c(1, 4)
+
+#c
+x - y
+# It is the difference between the elements of vector x and elements of vector y. So the first element of x is 4 and the first element of y is 1 and  the result is 3 and so on. Since vector y has no 3rd and 4th element the result is 0
+
+#d
+s <- c(x, y)
+
+#e
+rep(s, 10)
+
+length(s)
+
+#f
+rep (s, each = 3)
+
+#g
+seq(7, 21)
+#h
+length (seq(7, 21))
