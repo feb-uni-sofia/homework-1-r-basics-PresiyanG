09c980f678e02cb4bdbaaa2bdab7bcd59d8760a5
Presiyan <presian.georgiev@outlook.com>
2018-04-01 19:20:36 +0200

    asd

--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,30 @@
+#a
+xmin <-c(23.0, 20.5, 28.2, 20.3, 22.4, 17.2, 18.2)
+xmax <-c(25.0, 22.8, 31.2, 27.3, 28.4, 20.2, 24.1)
 
+#b
+xmax - xmin
+
+#c
+totalMax <- sum(xmax)
+totalMin <- sum(xmin)
+avgMax <- totalMax/length(xmax)
+avgMin <- totalMin/length(xmin)
+
+#d
+xmin[xmin<avgMin]
+
+#e
+xmin[xmax>avgMax]
+
+#f
+Date <-c('03Mon18', '04Tue18', '05Wed18', '04Thu18', '05Fri18', '06Sat18', '07Sun18')
+names(xmax) <- Date
+names(xmin) <- Date
+
+#g
+temperatures <- data.frame(df.xmax = xmax, df.xmin = xmin)
+  
+#h what
+
+#i
